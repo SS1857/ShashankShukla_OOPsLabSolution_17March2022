551ead751bb33096768720a1ca3e170e054ab2b4
SS1857 <89942518+SS1857@users.noreply.github.com>
2022-03-17 02:25:57 +0530

    Add files via upload

---
+++ Email-Application-Project/src/com/greatlearning/model/Employee.java
@@ -0,0 +1,65 @@
+package com.greatlearning.model;
+
+public class Employee {
+
+	 private String firstName;
+
+     private String lastName;
+
+    
+
+     //Default contructor
+
+     public Employee()  {
+
+     }
+
+
+
+//Parameterized constructor
+
+     public Employee (String firstName, String lastName) {
+
+         this.firstName = firstName;
+
+         this.lastName = lastName;
+
+     }
+
+    
+
+     // Setters
+
+     public void setFirstName(String firstName) {
+
+           this.firstName = firstName;
+
+     }
+
+    
+
+     public void setLastName(String lastName)   {
+
+         this.lastName = lastName;
+
+  }
+
+    
+
+     // Getters
+
+     public String getFirstName() {
+
+           return this.firstName;
+
+     }
+
+    
+
+     public String getLastName() {
+
+           return this.lastName;
+
+     }
+
+}
